79aeb44fd00b7c7ce4eacea96ca5454fff7dcdb8
Ahmadu TJ Abdullahi <AAbdullahi@rccl.com>
2019-05-14 19:06:05 -0400

    Completed Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,15 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
-import { ProductListComponent } from './product-list/product-list.component'
+import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path:'products', component: ProductListComponent},
+  {path:'product/:id', component: ProductPageComponent},
+  {path:'', redirectTo: 'products', pathMatch: 'full'}
+
+];
 
 @NgModule({
   declarations: [
@@ -21,7 +29,8 @@ import { ProductListComponent } from './product-list/product-list.component'
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     ProductService
